30330a55286757236ab926bb37c11eaf3d16100d
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-05 10:58:53 +0300

    feat: Added common appbar widget

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -2,12 +2,31 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-@RoutePage()    
+@RoutePage()
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Portfolio'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to my portfolio!',
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('About Me'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
