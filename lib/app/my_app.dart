0803e4bd48e1b00f65d3dff64f69b01a5214b9bc
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-08 11:36:53 +0300

    removed unused dependencies

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:my_portfolio/providers/dark_theme_provider.dart';
 import 'package:my_portfolio/routes/app_router.dart';
 
